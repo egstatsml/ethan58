104db6a2d9fbf5dea55e38900f47686b9884571b
Ethan Goan <ethanjgoan@gmail.com>
2022-08-02 21:34:06 +1000

    corrected keymap for left shift.

--- keymap.c
+++ keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,    KC_QUOT,
-  KC_LCTRL,LGUI_T(KC_Z),LALT_T(KC_X),LCTL_T(KC_C),LSFT_T(KC_V),KC_B,KC_LBRC,KC_RBRC, KC_N,RSFT_T(KC_M),RCTL_T(KC_COMM),RALT_T(KC_DOT),RGUI_T(KC_SLASH), KC_RSFT,
+  KC_LSFT,LGUI_T(KC_Z),LALT_T(KC_X),LCTL_T(KC_C),LSFT_T(KC_V),KC_B,KC_LBRC,KC_RBRC, KC_N,RSFT_T(KC_M),RCTL_T(KC_COMM),RALT_T(KC_DOT),RGUI_T(KC_SLASH), KC_RSFT,
            KC_LALT, LGUI_T(KC_MINS), LCTL_T(KC_SPC), LSFT_T(KC_TAB), TO(_NUMB), RCTL_T(KC_ENT), RGUI_T(KC_BSPC), KC_RALT
 ),
 /* NUMB
