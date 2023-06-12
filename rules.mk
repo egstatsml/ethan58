41e9f886fc4547b218c5b0921a48fde696379829
Ethan Goan <ethanjgoan@gmail.com>
2023-06-13 07:02:59 +1000

    removing lcd stuff

--- rules.mk
+++ rules.mk
@@ -8,14 +8,16 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE = yes     # OLED display
-WPM_ENABLE = yes
-
+OLED_ENABLE = no     # OLED display
+# WPM_ENABLE = yes
+# BOOTLOADER = rp2040
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
-        ./lib/mode_icon_reader.c \
-        ./lib/host_led_state_reader.c \
-        ./lib/timelogger.c \
+# SRC +=  ./lib/rgb_state_reader.c \
+#         ./lib/layer_state_reader.c \
+#         ./lib/logo_reader.c \
+#         ./lib/keylogger.c \
+#         ./lib/mode_icon_reader.c \
+#         ./lib/host_led_state_reader.c \
+#         ./lib/timelogger.c \
+
+# SRC += ./lib/layer_state_reader.c
